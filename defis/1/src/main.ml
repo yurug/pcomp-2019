46b1a9bd0bbe85b2249cf86e42b11766234e83ab
Antonin Décimo <antonin.decimo@gmail.com>
2019-01-18 17:41:10 +0100

    Better main?

--- defis/1/src/main.ml
+++ defis/1/src/main.ml
@@ -13,16 +13,14 @@ let to_things data_filename _ _ _ : unit =
 let parse_args args = args.(1), args.(2), args.(3), args.(4)
 
 let main () =
-  let error_message =
-    "Bad Input, should be \"data.csv user.txt view0.csv changes.txt\""
-  in
-  match Sys.argv with
-  | args when Array.length args = 5 ->
+  if Array.length Sys.argv = 5
+  then
     let data_filename, user_filename, view0_filename, changes_filename =
       parse_args Sys.argv
     in
     to_things data_filename user_filename view0_filename changes_filename
-  | exception Invalid_argument _ -> prerr_endline error_message
-  | _ -> prerr_endline error_message
+  else (
+    prerr_endline "Usage: ws <data.csv> <user.txt> <view0.csv> <changes.txt>";
+    exit 1 )
 
 let () = main ()
