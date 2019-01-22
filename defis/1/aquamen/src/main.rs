8916f67a298f5445b19fe33a706f85c063f4b590
n3f4s <tiv.crb@gmail.com>
2019-01-22 19:51:56 +0100

    Init dev directory
    Basic makefile calling cargo
    Directory architecture
    Basic Cargo.toml

--- defis/1/aquamen/src/main.rs
+++ defis/1/aquamen/src/main.rs
@@ -1,19 +1,3 @@
-#[macro_use]
-extern crate combine;
-
-mod data;
-mod sched;
-mod parser;
-mod interpreter;
-mod printer;
-mod process;
-
-use std::env;
-
-use sched::schedule;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    schedule(&args[1], &args[2], &args[3], &args[4]);
+    println!("Hello, world!");
 }
