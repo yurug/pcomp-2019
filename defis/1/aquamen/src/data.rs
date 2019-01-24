02170e55b55e3694107cfc7afa4b63df910b2c6c
Julien Rolland <julien.rolland@etu.univ-paris-diderot.fr>
2019-01-24 13:59:12 +0100

    Data types

--- defis/1/aquamen/src/data.rs
+++ defis/1/aquamen/src/data.rs
@@ -1,65 +1,18 @@
-use std::hash::{Hash, Hasher};
-
-pub type Num = u8 ;
-
-pub struct Matrix<T: Copy + Clone> {
-    inner: Vec<Vec<T>>
+struct Coordinates {
+    x: u32,
+    y: u32
 }
 
-impl<T: Copy + Clone> Matrix<T> {
-    pub fn from_2d_vec(v: Vec<Vec<T>>) -> Matrix<T> {
-        Matrix {
-            inner: v
-        }
-    }
-    // Assume that the ordering of cell is conserved
-    // aka mat.get(p).loc == p
-    pub fn get(&self, c: Point) -> T {
-        self.inner[c.x as usize][c.y as usize].clone()
-    }
-
-    pub fn lines(&self) -> &Vec<Vec<T>> {
-        &self.inner
-    }
+struct Cell {
+    content: Val,
+    location: Coordinates
 }
 
-/// For now we don't need requirements
-/// so the type is a "singleton" type
-/// to avoid to deal with passing Vec around.
-/// The type will be changed when we do need
-/// to split sheets
-// pub type Requirements = Vec<Cell>;
-pub enum Requirements { Empty }
-
-
-#[derive(Debug,Clone,Copy,PartialEq)]
-pub struct Point {
-    pub x: u64,
-    pub y: u64
+enum Val {
+    Raw(u8),
+    Fun(Function) 
 }
 
-#[derive(Debug,PartialEq,Copy,Clone)]
-pub struct Cell {
-    pub content: Data,
-    pub loc: Point
-}
-
-impl Hash for Point {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.x.hash(state);
-        self.y.hash(state);
-    }
-}
-impl Eq for Point {}
-
-#[derive(Debug,Clone,PartialEq,Copy)]
-pub enum Data {
-    Val(Num),
-    Fun(Function),
-    Wrong
-}
-
-#[derive(Debug,Clone,PartialEq,Copy)]
-pub enum Function {
-    Count(Point, Point, Num)
+enum Function {
+    Count(Coordinates, Coordinates, u8)
 }
--- defis/1/aquamen/src/main.rs
+++ defis/1/aquamen/src/main.rs
@@ -1,3 +1,5 @@
+mod data;
+
 fn main() {
     println!("Hello, world!");
 }
