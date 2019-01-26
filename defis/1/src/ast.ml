37231e15c8f06f228594b278c05e2a0e34436e30
KostiaChardonnet <kostia.chardonnet@yahoo.fr>
2019-01-26 15:33:41 +0100

    Update ast

--- defis/1/src/ast.ml
+++ defis/1/src/ast.ml
@@ -5,15 +5,20 @@ type pos =
 
 (* top left * bottom right *)
 type formula = Occ of (pos * pos) * value
+             | Val of value
 
 and value =
   | Undefined
   | Int of int
 
-type cell =
-  { value : value }
+type cell = {value : value}
 
-type action = Set of pos * cell
+type action = Set of pos * formula
 
 (* FIXME: ugly hack, needs workaround *)
 type spreadsheet = cell list list
+
+(* [value cell] return the field value of type value from [cell]. *)
+let value {value} = value
+
+let string_of_value = function Int i -> string_of_int i | _ -> "P"
