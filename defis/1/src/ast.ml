aecad72f90fac6e05035122fe83ce64ce4558a52
Antonin Décimo <antonin.decimo@gmail.com>
2019-01-26 14:11:45 +0100

    Cells don’t contain their formula

--- defis/1/src/ast.ml
+++ defis/1/src/ast.ml
@@ -3,16 +3,15 @@ type pos =
   { r : int
   ; c : int }
 
-type cell =
-  { value : value
-  ; formula : formula option }
-
 (* top left * bottom right *)
-and formula = Occ of (pos * pos) * value
+type formula = Occ of (pos * pos) * value
 
 and value =
   | Undefined
   | Int of int
+
+type cell =
+  { value : value }
 
 type action = Set of pos * cell
 
