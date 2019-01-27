75b399b34d018294c396963fc38bc01b8cfefcce
n3f4s <tiv.crb@gmail.com>
2019-01-27 23:17:45 +0100

    Implement eval function

--- defis/1/aquamen/src/process/eval.rs
+++ defis/1/aquamen/src/process/eval.rs
@@ -1,6 +1,40 @@
-mod data;
 
-// FIXME TODO
-fn eval(in: &Matrix<Cell>) -> Matrix<Num> {
-    unimplemented!();
+use data::{Matrix, Cell, Num, Point, Data};
+use data::Data::*;
+use data::Function::*;
+
+// FIXME test!!!!
+
+// FIXME should return a list of dependencies
+// in case of if we can't completly compute
+// the cell (because we don't have the full sheet)
+fn eval_cell(data: &Data, sheet: &Matrix<Cell>) -> Num {
+    match data {
+        Val(i) => i.clone(),
+        Fun(Count(b, e, v)) => {
+            let mut acc = 0;
+            for x in b.x..e.x {
+                for y in b.y..e.y {
+                    if eval_cell(&sheet.get(Point{x: x, y: y}).content, sheet) == v.clone() {
+                        acc += 1;
+                    }
+                }
+            }
+            acc
+        }
+        _ => 0 // FIXME better errors!!!
+
+    }
+}
+
+pub fn eval(input: &Matrix<Cell>) -> Matrix<Num> {
+    let mut res = Vec::new();
+    for line in input.lines() {
+        let mut lres: Vec<Num>  = Vec::with_capacity(line.len());
+        for cell in line {
+            lres.push(eval_cell(&cell.content, input))
+        }
+        res.push(lres)
+    }
+    Matrix::from_2d_vec(res)
 }
