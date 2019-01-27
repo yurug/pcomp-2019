370bd0e03834763a7c922ae7cf9fd8e350eaaa64
Antonin Décimo <antonin.decimo@gmail.com>
2019-01-27 16:26:43 +0100

    Simpler parser

--- defis/1/src/spreadsheet.ml
+++ defis/1/src/spreadsheet.ml
@@ -10,57 +10,32 @@ module Make (D : Data.DATA) = struct
 
   let parse_data data_filename =
     let ic = open_in data_filename in
-    let row, col = ref 0, ref 0 in
-    let d = ref 0 in
-    let state = ref Undef in
-    let read_occurences () =
-      let sic = Scanf.Scanning.from_channel ic in
-      let f r1 c1 r2 c2 v =
-        Ast.Occ (({r = r1; c = c1}, {r = r2; c = c2}), Int v)
-      in
-      Scanf.bscanf sic "(%d, %d, %d, %d, %d)" f
-    in
-    let read_formula graph =
-      let fml =
-        match input_char ic with
-        | '#' -> read_occurences ()
-        | _ -> failwith "Unsupported formula"
-      in
-      let node = Graph.build_node fml in
-      Graph.add_node graph Ast.{r = !row; c = !col} node
-    in
-    let rec read (data, graph) =
+    let rec aux (data, formulas) r c =
       try
-        (match input_char ic with
-        | '0' .. '9' as c ->
-          (match !state with
-          | Undef ->
-            d := (!d * 10) + int_of_char c;
-            state := Val
-          | Val -> d := (!d * 10) + int_of_char c
-          | _ -> failwith "Incorrect syntax");
-          data, graph
-        | ';' ->
-          let data =
-            match !state with
-            | Undef -> D.set {r = !row; c = !col} {value = Undefined} data
-            | Val -> D.set {r = !row; c = !col} {value = Int !d} data
-            | Formula -> data
-          in
-          incr col;
-          state := Undef;
-          data, graph
-        | '\n' ->
-          incr row;
-          col := 0;
-          state := Undef;
-          D.set {r = !row; c = !col} {value = Undefined} data, graph
-        | '=' -> data, read_formula graph
-        | _ -> failwith "Incorrect syntax")
-        |> read
-      with End_of_file -> data, graph
+        let line = input_line ic in
+        let cells = String.split_on_char ';' line in
+        List.fold_left
+          (fun (data, formulas) cell ->
+            try
+              let value = Scanf.sscanf cell "%d" (fun d -> d) in
+              let value = Ast.Int value in
+              D.set {r; c} {value} data, formulas
+            with Scanf.Scan_failure _ ->
+              (try
+                 let formula =
+                   Scanf.sscanf
+                     cell
+                     "=#(%d, %d, %d, %d %d)"
+                     (fun r1 c1 r2 c2 v ->
+                       Ast.Occ (({r = r1; c = c1}, {r = r2; c = c2}), Int v) )
+                 in
+                 data, formula :: formulas
+               with Scanf.Scan_failure str -> failwith str) )
+          (data, formulas)
+          cells
+      with End_of_file -> data, formulas
     in
-    read (D.create 16 16, Graph.empty)
+    aux (D.create 16 16, []) 0 0
 
   let output data view0 =
     let file = open_out view0 in
