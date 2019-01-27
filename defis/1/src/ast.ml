b7780384478beaf2f37e02909f873d7b67d55150
MOREL carine <morel@informatique.univ-paris-diderot.fr>
2019-01-27 14:38:50 +0100

    Ajout de ast.mli et ajout des fonctions utilitaires (d'affichage) dans ast.

--- defis/1/src/ast.ml
+++ defis/1/src/ast.ml
@@ -24,6 +24,19 @@ type spreadsheet = cell list list
 let value {value} = value
 let string_of_value = function Int i -> string_of_int i | _ -> "P"
 
+let string_of_pos {r; c} = "(" ^ string_of_int r ^ "," ^ string_of_int c ^ ")"
+
+let string_of_content content =
+  match content with
+  | Val v -> string_of_value v
+  | Occ ((p1, p2), v) ->
+    "Occ ("
+    ^ string_of_pos p1
+    ^ ", "
+    ^ string_of_pos p2
+    ^ "), "
+    ^ string_of_value v
+
 let compare_pos {r = r1; c = c1} {r = r2; c = c2} =
   let res = compare r1 r2 in
   if res = 0 then compare c1 c2 else res
---
+++ defis/1/src/ast.mli
@@ -0,0 +1,32 @@
+type pos =
+  { r : int
+  ; c : int }
+
+val pos : int -> int -> pos
+
+type content =
+  | Occ of (pos * pos) * value
+  | Val of value
+
+and value =
+  | Undefined
+  | Int of int
+
+type cell = {value : value}
+type action = Set of pos * content
+
+(* FIXME *)
+type spreadsheet = cell list list
+
+(* [value cell] return the field value of type value from [cell]. *)
+val value : cell ->  value
+val string_of_value : value -> string
+val string_of_pos : pos -> string
+val string_of_content : content -> string
+
+
+val compare_pos : pos -> pos -> int
+
+module Mpos : Map.S with type key = pos
+
+module Spos : Set.S with type elt = pos
