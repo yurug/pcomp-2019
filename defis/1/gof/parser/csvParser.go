f09e8913fbf86f2545646c117ee09d35d47ffb3a
Samy Metadjer <samy.metadjer@gmail.com>
2019-01-27 02:07:56 +0100

    feat(parser): parser for csv and user requests

---
+++ defis/1/gof/parser/csvParser.go
@@ -0,0 +1,12 @@
+package parser
+
+//ParseCsv TO-DO
+func ParseCsv(csv string) string {
+	/*
+		controller, err := db.NewController(csv)
+		if err != nil {
+			return ""
+			}
+	*/
+	return ""
+}
---
+++ defis/1/gof/parser/userParser.go
@@ -0,0 +1,12 @@
+package parser
+
+//ParseUserRequest TO-DO
+func ParseUserRequest(usrReq string) string {
+	/*
+		controller, err := db.NewController(usrReq)
+		if err != nil {
+			return ""
+			}
+	*/
+	return ""
+}
