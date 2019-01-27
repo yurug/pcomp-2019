5d7678782e9b77d68910229f774f5ba9f5aef8d6
Khawlamhb <mouhoubi.khawla@gmail.com>
2019-01-27 12:42:33 +0100

    écriture des fonctions de base de la feuille de calque simple

--- defis/1/madz/src/src/donnees/FeuilleCalque.scala
+++ defis/1/madz/src/src/donnees/FeuilleCalque.scala
@@ -3,7 +3,7 @@ package donnees
 // Représentation de la feuille de calque et accès lecture écriture case par case
 
 trait FeuilleCalque {
-  def loadCalc(file:String): Unit
+  def loadCalc(): Unit
   def getCell(r:Int , c:Int): Cellule
-  def writeCell(r:Int, c:Int, v:Int): Unit
+  def writeCell(r:Int, c:Int, v:String): Unit
 }
