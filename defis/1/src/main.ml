c0430574fbcc2abf635600e2e37d31a6c69681d1
Antonin Décimo <antonin.decimo@gmail.com>
2019-01-27 18:19:41 +0100

    Exit at the beginning if incorrect parameters

--- defis/1/src/main.ml
+++ defis/1/src/main.ml
@@ -7,26 +7,24 @@ module D = DataArray
 module Sp = Spreadsheet.Make (D)
 
 let main () =
-  if Array.length Sys.argv = 5
+  if Array.length Sys.argv <> 5
   then (
-    let data_filename, user_filename, view0_filename, changes_filename =
-      Sys.argv.(1), Sys.argv.(2), Sys.argv.(3), Sys.argv.(4)
-    in
-    (* Initialisation *)
-    let data, formulas = Sp.parse_data data_filename in
-    let graph = Sp.build_graph data formulas in
-    let user = open_in user_filename in
-    Sp.output data view0_filename;
-    let rec loop data graph =
-      let line = input_line user in
-      let action = Sp.parse_action line in
-      let data, graph = Sp.update data graph action in
-      loop data graph
-    in
-    try loop data graph with End_of_file ->
-      close_in user)
-  else (
     prerr_endline "Usage: ws <data.csv> <user.txt> <view0.csv> <changes.txt>";
-    exit 1 )
+    exit 1 );
+  let data_filename, user_filename, view0_filename, changes_filename =
+    Sys.argv.(1), Sys.argv.(2), Sys.argv.(3), Sys.argv.(4)
+  in
+  (* Initialisation *)
+  let data, formulas = Sp.parse_data data_filename in
+  let graph = Sp.build_graph data formulas in
+  let user = open_in user_filename in
+  Sp.output data view0_filename;
+  let rec loop data graph =
+    let line = input_line user in
+    let action = Sp.parse_action line in
+    let data, graph = Sp.update data graph action in
+    loop data graph
+  in
+  try loop data graph with End_of_file -> close_in user
 
 let () = main ()
