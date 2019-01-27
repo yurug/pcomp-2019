61c0449d84be9163c71cef3b2234a61afbf941f4
n3f4s <tiv.crb@gmail.com>
2019-01-27 11:50:43 +0100

    Add definition of schedule function

--- defis/1/aquamen/src/sched.rs
+++ defis/1/aquamen/src/sched.rs
@@ -1,20 +1,6 @@
 
-use std::sync::mpsc::channel;
-use std::fs;
-
-use process::process;
-
-// FIXME test!!!!
-
-pub fn schedule(sheet_path: &str, user_mod_path: &str, view0_path: &str, changes_path: &str) {
-
-    // Step 1
-
-    let sheet = fs::read_to_string(sheet_path)
-        .expect("Something went wrong reading the file");
-    let changes = fs::read_to_string(user_mod_path)
-        .expect("Something went wrong reading the file");
-
-    let (sender, _recv) = channel();
-    process(sheet, changes, view0_path, changes_path, sender);
+// FIXME TODO
+fn schedule(block: String) {
+    // Call function from process mod
+    unimplemented!();
 }
