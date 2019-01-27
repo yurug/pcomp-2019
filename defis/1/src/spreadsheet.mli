437023bf6ff858907359accf49cbcfea31c6ba88
Antonin Décimo <antonin.decimo@gmail.com>
2019-01-27 18:10:29 +0100

    A bit of doc

--- defis/1/src/spreadsheet.mli
+++ defis/1/src/spreadsheet.mli
@@ -5,17 +5,20 @@ module Make (D : Data.DATA) : sig
      spreadsheet. *)
   val parse_data : string -> data * (Ast.pos * Graph.node_content) list
 
+  (** [build_graph data formulas] creates a dependency graph from a
+     given [data] and list of [formulas]. *)
   val build_graph : data -> (Ast.pos * Graph.node_content) list -> Graph.t
 
   (** [parse_action line] creates a user action from [line], an user
      supplied action string. *)
   val parse_action : string -> Ast.action
 
-  (** [output tab view0.csv] write the contents of [tab]
+  (** [output data view0.csv] write the contents of [data]
       in the file [view0.csv], the file is created if it does not exist *)
   val output : data -> string -> unit
 
-  (* incremential action applied to the tab, given a dependency graph *)
+  (** [update data graph action] applies the action [action] to [data]
+     and updates [graph] accordingly.*)
   val update : data -> Graph.t -> Ast.action -> data * Graph.t
 
   (* evaluation of one cell *)
