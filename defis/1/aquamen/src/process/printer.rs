94b94146fabb206a7798776d32a2888a52e80241
n3f4s <tiv.crb@gmail.com>
2019-01-28 14:05:42 +0100

    Reflect change of result matrix type

--- defis/1/aquamen/src/process/printer.rs
+++ defis/1/aquamen/src/process/printer.rs
@@ -3,8 +3,14 @@ use data::{Matrix, Data};
 use data::Data::{Val, Wrong};
 
 // FIXME test!!!
-pub fn print(block: Matrix<Num>) {
+pub fn print(block: Matrix<Data>) {
     for line in block.lines() {
-        print!("{}\n",line.into_iter().map(|i| i.to_string()).collect::<Vec<String>>().join(";"))
+        print!("{}\n",line.into_iter().map(|i| {
+            match i {
+                Val(i) => i.to_string(),
+                Wrong => "P".to_owned(),
+                _ => panic!("Unexpected value while printing")
+            }
+        }).collect::<Vec<String>>().join(";"))
     }
 }
