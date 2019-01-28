b98987d6a426c2f0e6dace46a801d9bec8e9914a
ZHENG <yoichitakago@hotmail.fr>
2019-01-28 10:18:35 +0100

    parser

---
+++ defis/1/triplek/Parser.scala
@@ -0,0 +1,64 @@
+class Parser {
+
+  def splitData(data: String): String[] = {
+    return data.split(";")
+  }
+
+  def makeACell(s: String): Cell = {
+    try {
+      var i: Int = s.toInt
+      if(i <= 255 && i >= 0) { return new CellInt(i) }
+      return new ErrorCell
+    }
+    catch {
+      case e: Exception => { return new ErrorCell }
+    }
+  }
+
+  def makeBCell(s: String): Cell = {
+    if(s.endsWith(")")) {
+      var cleanedString = s.subString(3, s.length() - 2)
+      cleanedString = cleanedString.replaceAll(" ", "")
+      var ss = s.split(",")
+      if(l.length == 5) {
+        var r1, c1, r2, c2, v, i: Int = 0
+        for(i <- 0 until 5) {
+          try {
+            i match {
+              case 0 => r1 = ss[i].toInt
+              case 1 => c1 = ss[i].toInt
+              case 2 => r2 = ss[i].toInt
+              case 3 => c2 = ss[i].toInt
+              case 4 => v = ss[i].toInt
+            }
+          }
+          catch {
+            case e: Exception => { return new ErrorCell }
+          }
+        }
+        return new CellFormule(r1, c1, r2, c2, v)
+      }
+    }
+    return new ErrorCell
+  }
+
+  def tokenize(data: List[String], acc: List[Cell]): List[Cell] = {
+    data match {
+      case Nil => { return acc }
+      case h::t => {
+        if(s.startsWith("=#(")) { cell = makeBCell(s) }
+        else { cell = makeACell(s) }
+        t match {
+          case ErrorCell => { return tokenize t acc }
+          case _ => { return tokenize t cell::acc }
+        }
+      }
+    }
+  }
+
+  def parse(data: String) {
+    splitData: String[] =  splitData(data)
+    return tokenize data Nil
+  }
+
+}
