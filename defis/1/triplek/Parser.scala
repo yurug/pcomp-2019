781d7971d937f8727e36c41266f7f32c73c5748a
ZHENG <yoichitakago@hotmail.fr>
2019-01-28 15:51:21 +0100

    privatisation des méthodes de parser pour limiter les points d'entrée

--- defis/1/triplek/Parser.scala
+++ defis/1/triplek/Parser.scala
@@ -3,19 +3,19 @@ import scala.io.Source
 object Parser {
 
 // Returns the List of lines contained in fileName
-  def readFile(fileName: String): List[String] = {
+  private def readFile(fileName: String): List[String] = {
     return Source.fromFile(fileName).getLines.toList
   }
 
 // Return a List in which each line contained in data turned into its splitted version
 // using ";" as a separator
-  def splitData(data: List[String]): List[List[String]] = {
+  private def splitData(data: List[String]): List[List[String]] = {
     var sd: List[List[String]] = Nil
     for(line <- data) { sd = line.split(";").toList :: sd }
     return sd
   }
 
-  def tokenizeLineAux(splittedString: List[String], acc: List[Cell]): List[Cell] = {
+  private def tokenizeLineAux(splittedString: List[String], acc: List[Cell]): List[Cell] = {
     splittedString match {
       case Nil => { return acc }
       case h::t => { return tokenizeLineAux(t, acc :+ CellFactory.create(h)) }
@@ -23,18 +23,18 @@ object Parser {
   }
 
 // Returns the List of Cell corresponding to the given List of String
-  def tokenizeLine(splittedString: List[String]): List[Cell] = {
+  private def tokenizeLine(splittedString: List[String]): List[Cell] = {
     return tokenizeLineAux(splittedString, Nil)
   }
 
-  def tokenizeAux(splittedData: List[List[String]], acc: List[List[Cell]]): List[List[Cell]] = {
+  private def tokenizeAux(splittedData: List[List[String]], acc: List[List[Cell]]): List[List[Cell]] = {
     splittedData match {
       case Nil => { return acc }
       case h::t => { return tokenizeAux(t, tokenizeLine(h)::acc) }
     }
   }
 
-  def tokenize(splittedData: List[List[String]]): List[List[Cell]] = {
+  private def tokenize(splittedData: List[List[String]]): List[List[Cell]] = {
     return tokenizeAux(splittedData, Nil)
   }
 
