be498516ff9cff46215f393d079aad7f3bd7b5ec
MOREL carine <morel@informatique.univ-paris-diderot.fr>
2019-01-28 10:18:47 +0100

    correction test.sh

--- defis/1/tests/tests.sh
+++ defis/1/tests/tests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-for f in *; do
+for f in tests/*; do
     if [ -d "$f" ]; then
         echo "# Test ${f}"
-        ../ws "$f"/data.csv "$f"/user.txt "$f"/view0.csv "$f"/changes.txt
+        ./ws "$f"/data.csv "$f"/user.txt "$f"/view0.csv "$f"/changes.txt
         echo
     fi
 done
