d6832ff6c45da4908527c4a2a380f9ecdc3ded5b
ZHENG <yoichitakago@hotmail.fr>
2019-01-28 15:28:32 +0100

    version fonctionnelle

--- defis/1/triplek/Tableur.scala
+++ defis/1/triplek/Tableur.scala
@@ -8,6 +8,10 @@ class CellInt (private var entier:Int) extends Cell {
   //   if(i <= 255 && i >= 0) { return new CellInt(i) }
   //   return new ErrorCell
   // }
+
+  override def toString: String = {
+    return entier.toString
+  }
 }
 
 class CellFormula (private var r1:Int, private var r2:Int, private var c1:Int, private var c2:Int, private var v:Int) extends Cell {
@@ -21,6 +25,9 @@ class CellFormula (private var r1:Int, private var r2:Int, private var c1:Int, p
 
 class ErrorCell extends Cell {
   // def create(s: String): Cell = { return new ErrorCell }
+  override def toString: String = {
+    return "P"
+  }
 }
 
 
@@ -31,8 +38,15 @@ object CellFactory {
       // if(s.startsWith("=#")) { return CellFormula.create(s) }
       // else if(s.matches("[0-9]+")) { return CellInt.create(s) }
       if(s.startsWith("=#")) {
-        var cleanedString: String = s.replaceAll("(|)|=|#|\\s", "")
-        var ss: Array[String] = s.split(",")
+        // val regex = "\\(|\\)|=|#|\\s|\n".r
+        // var cleanedString: String = regex.replaceAllIn(s, "")
+        // var cleanedString: String = s.replaceAll("\\s | \\( | \\) | = | #", "")
+        var cleanedString: String = s.replaceAll("\\s", "")
+        cleanedString = cleanedString.replaceAll("\\(", "")
+        cleanedString = cleanedString.replaceAll("\\)", "")
+        cleanedString = cleanedString.replaceAll("=", "")
+        cleanedString = cleanedString.replaceAll("#", "")
+        var ss: Array[String] = cleanedString.split(",")
         if(ss.length == 5) { return new CellFormula(ss(0).toInt, ss(1).toInt, ss(2).toInt, ss(3).toInt, ss(4).toInt) }
       }
       else if(s.matches("[0-9]+")) {
---
+++ defis/1/triplek/Test.scala
@@ -0,0 +1,6 @@
+object Test {
+  def main(args: Array[String]) {
+    val t = Parser.parse("test.csv")
+    println(t)
+  }
+}
