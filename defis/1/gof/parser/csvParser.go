136f3eeffc8fc441a6a6f7614f73c1440f00426c
Samy Metadjer <samy.metadjer@gmail.com>
2019-01-29 18:57:24 +0100

    feat(parser): first code for csvParser with use of go-channel

--- defis/1/gof/parser/csvParser.go
+++ defis/1/gof/parser/csvParser.go
@@ -1,8 +1,28 @@
 package parser
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/yurug/pcomp-2019/defis/1/gof/db"
+	"github.com/yurug/pcomp-2019/defis/1/gof/eval"
+)
 
 //ParseCsv TO-DO
-func ParseCsv(csv string) string {
+func ParseSheet(sheet string, c chan eval.Cell) (string, error) {
+	controller, err := db.NewController(sheet)
+	if err != nil {
+		return "", fmt.Errorf("Error while calling new controller for ParseSheet: %v", err)
+	}
+
+	err = nil
+	for err == nil {
+		line, err := controller.NextLine()
+
+		c <- eval.NewNumber(5, 5, 5)
+
+	}
 	/*
 		controller, err := db.NewController(csv)
 		if err != nil {
