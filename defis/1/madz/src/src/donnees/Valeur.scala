73d174dc5cc1cd5d49a3af11093ead9ba47cc81a
zhenlei <zhenleic@gmail.com>
2019-01-30 20:55:16 +0100

    j'ai ajouter une proposition sur le systeme de evaluation

---
+++ defis/1/madz/src/src/donnees/Valeur.scala
@@ -0,0 +1,12 @@
+/*
+cette manière de faire est beaucoup mieux, je trouve
+def evalData (data: CaseData): Option[Value]
+evaluation donne 1 type Value
+def print val = val match{
+	case VUnknow => P
+	case VInt v => ..
+}
+*/
+trait Value
+case class VInt(v : Int) extends Int with Value 
+case class VUnknow extends Value
