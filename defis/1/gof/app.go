4e7287a7203054726608f57be5b30468734a9f7a
Samy Metadjer <samy.metadjer@gmail.com>
2019-02-01 15:24:00 +0100

    feat(app): first version launchable, for further tests

--- defis/1/gof/app.go
+++ defis/1/gof/app.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/yurug/pcomp-2019/defis/1/gof/eval"
+	"github.com/yurug/pcomp-2019/defis/1/gof/parseutil"
+)
+
+func main() {
+	filename := os.Args[1]
+	ch := make(chan eval.Cell)
+	go parserutil.ParseSheet(filename, ch)
+	for v := range ch {
+		switch cell := v.(type) {
+		default:
+			fmt.Printf("unexpected type %T", cell)
+		case *eval.Number:
+			fmt.Printf("Cell of type Number and value: %v\n", cell.Value)
+		case *eval.Unknown:
+			fmt.Printf("Cell of type Unknown and value: %v\n", cell.Value())
+		case *eval.Formula:
+			fmt.Printf("Cell of type Formula: %v\n", "Not yet")
+		}
+	}
 }
