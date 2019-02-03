adbe5eecb6653df7eee0ab6964f3ea95d99f2c88
Samy Metadjer <samy.metadjer@gmail.com>
2019-02-04 00:06:29 +0100

    refactor(app): remove useless code and comments

--- defis/1/gof/app.go
+++ defis/1/gof/app.go
@@ -8,7 +8,6 @@ import (
 	"github.com/yurug/pcomp-2019/defis/1/gof/parseutil"
 )
 
-//"./ws data.csv user.txt view0.csv changes.txt"
 func main() {
 	args := os.Args[1:]
 	if len(args) < 4 {
@@ -26,21 +25,4 @@ func main() {
 	}
 	go parserutil.ParseSheet(csv, ch)
 	e.Process(ch)
-}
-
-func test(ch chan []eval.Cell) {
-	for line := range ch {
-		for _, v := range line {
-			switch cell := v.(type) {
-			default:
-				fmt.Printf("unexpected type %T", cell)
-			case *eval.Number:
-				fmt.Printf("Cell of type Number and value: %v\n", cell.Value())
-			case *eval.Unknown:
-				fmt.Printf("Cell of type Unknown and value: %v\n", cell.Value())
-			case *eval.Formula:
-				fmt.Printf("Cell of type Formula: %v / %v / %v\n", cell.Start, cell.End, cell.ToEval)
-			}
-		}
-	}
 }
