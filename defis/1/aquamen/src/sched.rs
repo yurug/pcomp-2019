df2a3361fa3c5a8714314539ccb02de1c93c2fb9
n3f4s <tiv.crb@gmail.com>
2019-02-04 00:46:31 +0100

    Remove code from sched that should be in process

--- defis/1/aquamen/src/sched.rs
+++ defis/1/aquamen/src/sched.rs
@@ -1,13 +1,8 @@
 
+use std::sync::mpsc::channel;
 use std::fs;
-use std::cmp::Ordering;
 
-use parser::parse_line;
-use interpreter::*;
-use printer::*;
-
-use data::Matrix;
-use data::Cell;
+use process::process;
 
 // FIXME test!!!!
 
@@ -15,49 +10,10 @@ pub fn schedule(sheet_path: &str, user_mod_path: &str, view0_path: &str, changes
 
     // Step 1
 
-    let buffer = fs::read_to_string(sheet_path)
+    let sheet = fs::read_to_string(sheet_path)
         .expect("Something went wrong reading the file");
-
-    let spreadsheet = build_spreadsheet(buffer);
-
-    let spreadsheet = eval(&spreadsheet);
-
-    print_spreadsheet(&spreadsheet, view0_path);
-
-
-    //Step 2
-
-    let buffer = fs::read_to_string(user_mod_path)
+    let changes = fs::read_to_string(user_mod_path)
         .expect("Something went wrong reading the file");
-
-    let changes = build_changes(buffer);
-
-    let mut changes = eval_changes(&changes, &spreadsheet);
-
-    changes.sort_by(|c1, c2| cell_cmp(&c1, &c2));
-
-    print_changes(changes, changes_path);
-}
-
-// FIXME for now we are going to assume that we
-// receive the full sheet. This should be modified
-// so that it can take an arbitrary rectangle view
-// of the spreadsheet
-fn build_spreadsheet(buffer: String) -> Matrix<Cell> {
-    // FIXME setup the channel
-    // FIXME setup a way to get data from central authority?
-    Matrix::from_2d_vec(buffer.split("\n").map(|l| parse_line(0, l)).collect())
-}
-
-
-
-fn build_changes(buffer: String) -> Vec<Cell> {
-    Vec::new() // TODO Hugo
-}
-
-fn cell_cmp(c1: &Cell, c2: &Cell) -> Ordering {
-    match c1.loc.x.cmp(&c2.loc.x) {
-        Ordering::Equal => c1.loc.y.cmp(&c2.loc.y),
-        o => o
-    }
+    let (sender, _recv) = channel();
+    process(sheet, changes, view0_path, changes_path, sender);
 }
