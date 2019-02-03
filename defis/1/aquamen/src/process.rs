c961016e3d33bdffa1c45fc3ab718756311caa0c
n3f4s <tiv.crb@gmail.com>
2019-02-04 00:46:04 +0100

    Add process function handling one thread
    This function takes care of everything that is happening in a thread. It
    will be called by the scheduler and should be able to send and receive
    messages from and to the scheduler. Each thread will have to start only
    this function.

---
+++ defis/1/aquamen/src/process.rs
@@ -0,0 +1,51 @@
+use std::sync::mpsc::Sender;
+use std::cmp::Ordering;
+
+use parser::parse_line;
+use parser::parse_change;
+use interpreter::*;
+use printer::*;
+
+use data::Matrix;
+use data::Cell;
+
+type Requirement = Cell;
+
+// FIXME add queue from scheduler to the process for cells
+// FIXME add queue from scheduler to the process for changes???
+pub fn process(spreadsheet_str: String,
+               user_mod: String,
+               view0_path: &str,
+               changes_path:&str,
+               _channel: Sender<Requirement>) {
+
+    let sheet = build_spreadsheet(spreadsheet_str);
+    let view0 = eval(&sheet);
+    print_spreadsheet(&view0, view0_path);
+
+    //Step 2
+    let changes = build_changes(user_mod);
+    let mut changes = eval_changes(&changes, &sheet);
+    changes.sort_by(|c1, c2| cell_cmp(&c1, &c2));
+    print_changes(changes, changes_path);
+}
+
+fn cell_cmp(c1: &Cell, c2: &Cell) -> Ordering {
+    match c1.loc.x.cmp(&c2.loc.x) {
+        Ordering::Equal => c1.loc.y.cmp(&c2.loc.y),
+        o => o
+    }
+}
+
+
+// FIXME for now we are going to assume that we
+// receive the full sheet. This should be modified
+// so that it can take an arbitrary rectangle view
+// of the spreadsheet
+fn build_spreadsheet(buffer: String) -> Matrix<Cell> {
+    Matrix::from_2d_vec(buffer.split("\n").map(|l| parse_line(0, l)).collect())
+}
+
+fn build_changes(buffer: String) -> Vec<Cell> {
+    buffer.split("\n").map(|l| parse_change(l)).collect()
+}
