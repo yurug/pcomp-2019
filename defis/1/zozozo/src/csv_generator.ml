00898e27182589e96e019183e2f24e055889063c
Antonin Décimo <antonin.decimo@gmail.com>
2019-02-03 19:46:41 +0100

    Fix ident, doc

--- defis/1/zozozo/src/csv_generator.ml
+++ defis/1/zozozo/src/csv_generator.ml
@@ -3,18 +3,19 @@ open Random
 (* [FIXME] : how to avoid code duplication ? *)
 let new_csv_file n =
   let folder = Printf.sprintf "tests/gen_%d" n in
-  try Unix.mkdir folder 0o755;
-      let s = Printf.sprintf "tests/gen_%d/data.csv" n in
-      open_out s
-  with _ -> let s = Printf.sprintf "tests/gen_%d/data.csv" n in
-      open_out s
+  try
+    Unix.mkdir folder 0o755;
+    let s = Printf.sprintf "tests/gen_%d/data.csv" n in
+    open_out s
+  with _ ->
+    let s = Printf.sprintf "tests/gen_%d/data.csv" n in
+    open_out s
 
 let new_change_file n =
   let s = Printf.sprintf "tests/gen_%d/change.txt" n in
   open_out s
 
-let generate_size min max =
-  (int max) + min
+let generate_size min max = int max + min
 
 let generate_formula a b =
   let a, b = a / 2, b / 2 in
@@ -23,17 +24,15 @@ let generate_formula a b =
   let v = int 256 in
   Printf.sprintf "=#(%d, %d, %d, %d, %d)" x y x' y' v
 
-let generate_value () =
-  string_of_int (int 256)
+let generate_value () = string_of_int (int 256)
 
 (* [FIXME] : latter switch between either a formula or a value  *)
-let generate_element ()  =
-  generate_value ()
+let generate_element () = generate_value ()
 
 let generate_line n =
   let rec aux acc = function
     | 0 -> acc
-    | n -> generate_element () :: aux acc (n-1)
+    | n -> generate_element () :: aux acc (n - 1)
   in
   aux [] n
 
@@ -42,14 +41,14 @@ let write_line file sep l =
   Printf.fprintf file "%s\n" s
 
 let generate_size max n =
-  let x = (32+(n-1)*(31191/max-1)) / 2 in
+  let x = (32 + ((n - 1) * ((31191 / max) - 1))) / 2 in
   x, x
 
 let rec write_file out sep lines = function
   | 0 -> close_out out
   | n ->
-     write_line out sep (generate_line lines);
-     write_file out sep lines (n - 1)
+    write_line out sep (generate_line lines);
+    write_file out sep lines (n - 1)
 
 let write_files csv change lines columns =
   write_file csv ";" lines columns;
@@ -58,12 +57,12 @@ let write_files csv change lines columns =
 let rec generator max formula = function
   | 0 -> ()
   | n ->
-     let out = new_csv_file n in
-     let lines, columns = generate_size max n in
-     let change = new_change_file n in
-     write_files out change lines columns;
-     generator max formula (n-1)
+    let out = new_csv_file n in
+    let lines, columns = generate_size max n in
+    let change = new_change_file n in
+    write_files out change lines columns;
+    generator max formula (n - 1)
 
-let generate_csv ?formula:(formula = false) n =
+let generate_csv ?(formula = false) n =
   self_init ();
   generator n formula n
--- defis/1/zozozo/src/csv_generator.mli
+++ defis/1/zozozo/src/csv_generator.mli
@@ -1,11 +1,11 @@
-(* [generate_csv b n] will genere [n] csv file in the folder tests/gen_X called
-   data.csv. Where gen_X is the x-th generated folder
-   The folders gen_X will be created if it does not exists.
-   Each folder will also contain a change.txt
+(* [generate_csv b n] generates [n] `data.csv` files in each
+   `tests/gen_X` directory, where gen_X is the x-th generated
+   directory. The directory will be created if it does not exists.
+   Each directory will also contain a `changes.txt` file.
 
-   if [b] is set to true, then some random formulas will be placed inside the csv.
-   Otherwise, the csv file will only contains values ranging from 0 to 255 (inclusive)
+   If [b] is true, then some random formulas will be placed inside the
+   csv. Otherwise, the csv file will only contains values ranging
+   from 0 to 255 (inclusive).
 
-  [FIXME] : formula generation hasn't been properly implemented yet
- *)
-val generate_csv ?formula:bool -> int -> unit
+  [FIXME]: formula generation hasn't been properly implemented yet *)
+val generate_csv : ?formula:bool -> int -> unit
