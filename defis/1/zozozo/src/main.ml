92cf6da9258baae4c51819ee54471ead7aa8e24d
MOREL carine <morel@informatique.univ-paris-diderot.fr>
2019-02-03 11:46:16 +0100

    Ajout d'affichage du temps dexecution avec option verbose

--- defis/1/zozozo/src/main.ml
+++ defis/1/zozozo/src/main.ml
@@ -7,24 +7,45 @@ module D = DataArray
 module Sp = Spreadsheet.Make (D)
 
 let parse_input argv =
-  if Array.length argv <> 5
-  then (
+  if Array.length argv = 5
+  then argv.(1), argv.(2), argv.(3), argv.(4), false
+  else if Array.length argv = 6
+  then argv.(1), argv.(2), argv.(3), argv.(4), argv.(5) = "--verbose"
+  else (
     prerr_endline "Usage: ws <data.csv> <user.txt> <view0.csv> <changes.txt>";
-    exit 1 );
-  argv.(1), argv.(2), argv.(3), argv.(4)
+    exit 1 )
 
 (*
   let _ = Format.eprintf "%s %s %s %s@."
       data_filename user_filename view0_filename changes_filename in
   data_filename, user_filename, view0_filename, changes_filename*)
 
+let print_execution_time t0 tinit tprintv0 tuser =
+  Format.eprintf "Global execution time (real): @.";
+  Format.eprintf "  Initialisation : %f s @." (tinit -. t0);
+  Format.eprintf "  Print view0    : %f s @." (tprintv0 -. tinit);
+  Format.eprintf "  Loop changes   : %f s @." (tuser -. tprintv0);
+  Format.eprintf "Total            : %f s @." (tuser -. t0)
+
+let print_initialisation_time t0 tinit tgraph teval =
+  Format.eprintf "Initialisation execution time (real): @.";
+  Format.eprintf "  Data init  : %f s @." (tinit -. t0);
+  Format.eprintf "  Graph init : %f s @." (tgraph -. tinit);
+  Format.eprintf "  First eval : %f s @." (teval -. tgraph);
+  Format.eprintf "Total        : %f s \n @." (teval -. t0)
+
 let pre_evaluation data graph formulas = Sp.eval_init data graph formulas
 
-let initialisation data_filename =
+let initialisation data_filename ~verbose =
+  let t0 = Unix.gettimeofday () in
   let data, formulas = Sp.init data_filename in
+  let t1 = Unix.gettimeofday () in
   let graph = Sp.build_graph formulas in
+  let t2 = Unix.gettimeofday () in
   let data = pre_evaluation data graph formulas in
-  data, Sp.build_graph formulas, formulas
+  let t3 = Unix.gettimeofday () in
+  if verbose then print_initialisation_time t0 t1 t2 t3;
+  data, graph, formulas
 
 let write_view0 view0_filename data = Sp.output data view0_filename
 
@@ -57,12 +78,17 @@ let loop_user user_filename changes_filename data graph =
   data
 
 let main () =
-  let data_filename, user_filename, view0_filename, changes_filename =
+  let data_filename, user_filename, view0_filename, changes_filename, verbose =
     parse_input Sys.argv
   in
-  let data, graph, _ = initialisation data_filename in
+  let t0 = Unix.gettimeofday () in
+  let data, graph, _ = initialisation data_filename ~verbose in
+  let t1 = Unix.gettimeofday () in
   write_view0 view0_filename data;
+  let t2 = Unix.gettimeofday () in
   let data = loop_user user_filename changes_filename data graph in
+  let t3 = Unix.gettimeofday () in
+  if verbose then print_execution_time t0 t1 t2 t3;
   (* debug *)
   write_final_result view0_filename data
 
--- defis/1/zozozo/tests/tests.sh
+++ defis/1/zozozo/tests/tests.sh
@@ -3,7 +3,7 @@
 for f in tests/*; do
     if [ -d "$f" ]; then
         echo "# Test ${f}"
-        ./ws "$f"/data.csv "$f"/user.txt "$f"/view0.csv "$f"/changes.txt
+        ./ws "$f"/data.csv "$f"/user.txt "$f"/view0.csv "$f"/changes.txt --verbose
         echo
     fi
 done
