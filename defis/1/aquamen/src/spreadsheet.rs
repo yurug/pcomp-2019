6fe9e61375d5f83cbe3815a90466a99a80b73400
Hugo Pompougnac <hugo.pompougnac@etu.univ-paris-diderot.fr>
2019-02-06 14:23:17 +0100

    premier TU

--- defis/1/aquamen/src/spreadsheet.rs
+++ defis/1/aquamen/src/spreadsheet.rs
@@ -1,4 +1,6 @@
-use data::{Cell,Data} ;
+use data::{Cell,Data,Point} ;
+use data::Data::{Val,Fun,Wrong} ;
+use data::Function::Count ;
 
 pub struct Spreadsheet {
 }
@@ -18,7 +20,7 @@ impl Spreadsheet {
         panic!("Impl missing")
     }
 
-    pub fn eval(&mut self, x : u64, y : u64) -> Cell {
+    pub fn eval(&mut self, p : Point) -> Cell {
         panic!("Impl missing")
     }
     
@@ -30,4 +32,38 @@ impl Spreadsheet {
     pub fn changes(&mut self) -> Vec<Cell> {
         panic!("Impl missing")
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn basic_guinea_pig () -> Spreadsheet {
+        let num = 5 ;
+        let r = (Point{x:0,y:0},Point{x:2,y:0});
+        let cells = vec![
+            vec![Cell{content:Val(num),loc:Point{x:0,y:0}},
+                 Cell{content:Val(8),loc:Point{x:1,y:0}},
+                 Cell{content:Val(num),loc:Point{x:2,y:0}}],
+            vec![Cell{content:Val(2),loc:Point{x:0,y:1}},
+                 Cell{content:Val(2),loc:Point{x:1,y:1}},
+                 Cell{content:Fun(Count(r.0,r.1,num)),loc:Point{x:2,y:1}}]
+        ];
+        let mut spreadsheet = Spreadsheet::new(3) ;
+        for line in cells.iter() {
+            for c in line.iter() {
+                spreadsheet.add_cell(*c) ;
+            }
+        }
+        spreadsheet
+    }
+
+    #[test]
+    fn test_simple_val() {
+        let mut spreadsheet = basic_guinea_pig() ;
+        let p = Point{x:0,y:1} ;
+        assert_eq!(Cell{content:Val(2),loc:p}, spreadsheet.eval(p)) ;
+    }
+
 }
