405a0e0aebb4d900b1dfea4bccadaddf4f23c1d6
Samy Metadjer <samy.metadjer@gmail.com>
2019-02-10 23:32:08 +0100

    feat(eval): visited tag in Cells added

--- defis/1/gof/eval/cell.go
+++ defis/1/gof/eval/cell.go
@@ -10,6 +10,8 @@ const BAD_FORMAT = "P"
 type Cell interface {
 	Coordinate() Coordinate
 	Value() string
+	Visited() bool
+	MarkVisit()
 }
 
 type Coordinate struct {
@@ -19,6 +21,7 @@ type Coordinate struct {
 type Number struct {
 	position Coordinate
 	value    int
+	visited  bool
 }
 
 func NewNumber(x int, y int, v int) (*Number, error) {
@@ -28,6 +31,7 @@ func NewNumber(x int, y int, v int) (*Number, error) {
 	return &Number{
 		position: Coordinate{x, y},
 		value:    v,
+		visited:  false,
 	}, nil
 }
 
@@ -39,11 +43,21 @@ func (n *Number) Value() string {
 	return strconv.Itoa(n.value)
 }
 
+func (n *Number) MarkVisit() {
+	n.visited = true
+}
+
+func (n *Number) Visited() bool {
+	return n.visited
+}
+
 type Formula struct {
 	position Coordinate
 	Start    Coordinate
 	End      Coordinate
 	ToEval   int
+	FinalV   int
+	visited  bool
 }
 
 func NewFormula(r1 int, c1 int, r2 int, c2 int, v int, x int, y int) *Formula {
@@ -52,6 +66,8 @@ func NewFormula(r1 int, c1 int, r2 int, c2 int, v int, x int, y int) *Formula {
 		Start:    Coordinate{r1, c1},
 		End:      Coordinate{r2, c2},
 		ToEval:   v,
+		FinalV:   -1,
+		visited:  false,
 	}
 }
 
@@ -60,21 +76,42 @@ func (f *Formula) Coordinate() Coordinate {
 }
 
 func (f *Formula) Value() string {
+	if f.FinalV != -1 {
+		return strconv.Itoa(f.FinalV)
+	}
 	return "F"
+}
+
+func (f *Formula) MarkVisit() {
+	f.visited = true
+}
+
+func (f *Formula) Visited() bool {
+	return f.visited
 }
 
 type Unknown struct {
 	position Coordinate
+	visited  bool
 }
 
 func NewUnknown(x int, y int) *Unknown {
 	return &Unknown{
 		position: Coordinate{x, y},
+		visited:  false,
 	}
 }
 
 func (u *Unknown) Coordinate() Coordinate {
 	return u.position
+}
+
+func (u *Unknown) MarkVisit() {
+	u.visited = true
+}
+
+func (u *Unknown) Visited() bool {
+	return u.visited
 }
 
 func (u *Unknown) Value() string {
