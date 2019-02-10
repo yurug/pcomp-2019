87214abc36452c687b201e2df32388c615eaa968
zhenlei <zhenleic@gmail.com>
2019-02-10 23:38:38 +0100

    main function can load request from file, then write result in file

--- defis/1/madz/src/src/donnees/Estimate_change.scala
+++ defis/1/madz/src/src/donnees/Estimate_change.scala
@@ -1,16 +1,24 @@
 package donnees
 
-class Estimate_change(case_toChange: Case, expression : CaseData) 
+case class Change(case_leftTop:Case, 
+	case_bottomRight:Case,
+	value: Value)
+	
+	class Estimate_change(case_toChange: Case, expression : CaseData) 
 extends Request_change(case_toChange, expression){ 
+  
+  protected var result: List[Change] = Nil 
   
   def this (c: Case, value : CaseData, sheet: FeuilleCalque) =
     this(c, value)
     this.set_sheet(sheet)
     
-  def exec():Unit = { update_case(case_toChange)}
+  def exec():Unit = { result = estimate_update_case(case_toChange)}
   /*
    * update case value, then update value of its dependance case*/
-  private def update_case(c : Case) = { /*
+  private def estimate_update_case(c : Case) : List[Change] = { 
+    throw new Exception("no implement")
+    /*
     val init_val = DataInterpreteur.evalData(c)
     val result = Interpreteur.evalData(c, c,:Int,view0:String)
     if (init != current) {
@@ -19,5 +27,7 @@ extends Request_change(case_toChange, expression){
       c =>     
     )}   */   
   }
-  def result_exec():String = ""  
+  def get_result = result
+    
+    
 }
--- defis/1/madz/src/src/donnees/Task.scala
+++ defis/1/madz/src/src/donnees/Task.scala
@@ -2,5 +2,4 @@ package donnees
 
 trait Task {
   def exec():Unit
-  def result_exec():String
 }
