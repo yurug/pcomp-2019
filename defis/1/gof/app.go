cd611dedb8acb974f6e2c9a916c06708ddce17e3
Mohammed Ansari <mohammed.ansari@hotmail.fr>
2019-02-10 14:24:16 +0100

    adding main

--- defis/1/gof/app.go
+++ defis/1/gof/app.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/yurug/pcomp-2019/defis/1/gof/eval"
-	parserutil "github.com/yurug/pcomp-2019/defis/1/gof/parseutil"
+	"github.com/yurug/pcomp-2019/defis/1/gof/db"
 )
 
 func main() {
@@ -14,14 +13,25 @@ func main() {
 		fmt.Printf("Not enough arguments\n")
 		return
 	}
-	csv := args[0]
-	//user := args[1]
-	//	view := args[2]
-	//changes := args[3]
-	ch := make(chan eval.Formula)
-	chbreak := make(chan int)
-	defer close(chbreak)
-	go parserutil.ParseSheet(csv, ch, chbreak)
-	<-chbreak
+	//	csv := args[0]
+	user := args[1]
+	view := args[2]
 
+	//changes := args[3]
+	/*
+		ch := make(chan eval.Formula)
+		chbreak := make(chan int)
+		defer close(chbreak)
+		go parserutil.ParseSheet(csv, ch, chbreak)
+		<-chbreak
+	*/
+
+	f, _ := db.NewFileModifier(view, user)
+	g, _ := f.GetValue(3, 1)
+	fmt.Println(g)
+
+	f.WriteValue(10, 10, 10)
+	g, _ = f.GetValue(10, 10)
+
+	fmt.Println(g)
 }
