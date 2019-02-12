4a6031a6457592166677aa497d92993be9c6ee63
Hugo Pompougnac <hugo.pompougnac@etu.univ-paris-diderot.fr>
2019-02-12 23:21:00 +0100

    un seul open

--- defis/2/aquamen/src/aprinter.rs
+++ defis/2/aquamen/src/aprinter.rs
@@ -1,6 +1,6 @@
 use data::{Index,Cell,Data,Point};
 use data::Data::{Val,Wrong,Fun};
-use std::fs::{OpenOptions,remove_file,read_to_string};
+use std::fs::{File,OpenOptions,remove_file,read_to_string};
 use std::io::{Write,Seek,SeekFrom,Error};
 
 const NUM_SIZE_IN_BYTES : Index = 3 ;
@@ -11,6 +11,7 @@ const WRONG_CHAR : u8 = 'p' as u8 ;
 pub struct APrinter {
     target_path : String,
     change_path : String,
+    target : File,
     cells_by_line : Index,
     bytes_by_line : Index,
 }
@@ -23,13 +24,16 @@ impl APrinter {
             cells_by_line * NUM_SIZE_IN_BYTES
         // place pour les séparateurs
             + cells_by_line ;
+        let t = OpenOptions::new()
+            .read(true).write(true).create(true).truncate(true)
+            .open(tp.clone()).unwrap();
         let p = APrinter {
             target_path : tp,
             change_path : cp,
+            target : t,
             cells_by_line : cells_by_line,
             bytes_by_line : bbl,
         } ;
-        p.clean() ;
         p
     }
 
@@ -48,7 +52,7 @@ impl APrinter {
         }
     }
     
-    pub fn print(&self, cell:Cell) {
+    pub fn print(&mut self, cell:Cell) {
         let x = cell.loc.x ;
         let y = cell.loc.y ;
         if x >= self.cells_by_line {
@@ -68,24 +72,18 @@ impl APrinter {
         self.stamp(offset, bytes);
     }
 
-    fn stamp(&self, offset : Index, text : Vec<u8>) {
-        let mut file = OpenOptions::new()
-            .read(true).write(true).create(true)
-            .open(self.target_path.clone()).unwrap();
+    fn stamp(&mut self, offset : Index, text : Vec<u8>) {
+        
         let future_offset = offset + NUM_SIZE_IN_BYTES ;
-        let f_len = file.metadata().unwrap().len();
+        let f_len = self.target.metadata().unwrap().len();
         if f_len <= future_offset {
-            self.io_may_crash(&mut || file.set_len(future_offset + 1)) ;
+            self.target.set_len(future_offset + 1) ;
         }
-        self.io_may_crash(&mut || {
-            match file.seek(SeekFrom::Start(offset)) {
-                Ok(_) => file.write_all(text.as_slice()),
-                Err(e) => Err(e),
-            }
-        });
+        self.target.seek(SeekFrom::Start(offset)) ;
+        self.target.write_all(text.as_slice()) ;
     }
 
-    pub fn print_changes(&self, effects: Vec<(Cell, Vec<Cell>)>) {
+    pub fn print_changes(&mut self, effects: Vec<(Cell, Vec<Cell>)>) {
 
         let mut file = OpenOptions::new()
             .read(true).write(true).create(true)
@@ -97,25 +95,25 @@ impl APrinter {
             tmp.append(&mut self.raw_change(change)) ;
             tmp.append(&mut "\":".as_bytes().to_vec()) ;
             tmp.push(EOL_SEP) ;
-            self.io_may_crash(&mut || file.write_all(tmp.as_slice()));
+            file.write_all(tmp.as_slice());
 
             for c in consequences {
                 tmp.clear() ;
                 tmp.append(&mut self.raw_change(c)) ;
                 tmp.push(EOL_SEP) ;
-                self.io_may_crash(&mut || file.write_all(tmp.as_slice())) ;
+                file.write_all(tmp.as_slice()) ;
             }
         }
     }
 
-    fn io_may_crash<F>(&self, f:&mut F)
-    where F: FnMut() -> Result<(),Error> {
-        let r = f() ;
-        if r.is_err() {
-            self.clean() ;
-            panic!(r.unwrap_err()) ;
-        }
-    }
+    // fn io_may_crash<F>(&mut self, f:&mut F)
+    // where F: FnMut() -> Result<(),Error> {
+    //     let r = f() ;
+    //     if r.is_err() {
+    //         self.clean() ;
+    //         panic!(r.unwrap_err()) ;
+    //     }
+    // }
     
     fn raw_change(&self, change : Cell) -> Vec<u8> {
         let mut preffix = format!("{} {} ",change.loc.y,change.loc.x)
@@ -169,7 +167,7 @@ mod tests {
 
     #[test]
     fn test_dummest_output() {
-        let printer = APrinter::new("u0".to_string(),"c0".to_string(),1);
+        let mut printer = APrinter::new("u0".to_string(),"c0".to_string(),1);
         printer.print(Cell{content:Val(2),loc:Point{x:0,y:0}});
         let content = read_to_string("u0").unwrap();
         printer.clean();
@@ -179,13 +177,13 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_rectangle_consistency() {
-        let printer = APrinter::new("u0b".to_string(),"c0b".to_string(),1);
+        let mut printer = APrinter::new("u0b".to_string(),"c0b".to_string(),1);
         printer.print(Cell{content:Val(2),loc:Point{x:3,y:0}});
     }
 
     #[test]
     fn test_two_columns() {
-        let printer = APrinter::new("u1".to_string(),"c1".to_string(),2);
+        let mut printer = APrinter::new("u1".to_string(),"c1".to_string(),2);
         printer.print(Cell{content:Val(2),loc:Point{x:0,y:0}});
         printer.print(Cell{content:Val(14),loc:Point{x:1,y:0}});
         let content = read_to_string("u1").unwrap();
@@ -195,7 +193,7 @@ mod tests {
 
     #[test]
     fn test_minimal_matrix() {
-        let printer = APrinter::new("u2".to_string(),"c2".to_string(),2);
+        let mut printer = APrinter::new("u2".to_string(),"c2".to_string(),2);
         printer.print(Cell{content:Val(2),loc:Point{x:0,y:0}});
         printer.print(Cell{content:Val(14),loc:Point{x:1,y:0}});
         printer.print(Cell{content:Val(100),loc:Point{x:0,y:1}});
@@ -207,7 +205,7 @@ mod tests {
 
     #[test]
     fn test_unordered_print() {
-        let printer = APrinter::new("u3".to_string(),"c3".to_string(),2);
+        let mut printer = APrinter::new("u3".to_string(),"c3".to_string(),2);
         printer.print(Cell{content:Val(100),loc:Point{x:0,y:1}});
         printer.print(Cell{content:Val(14),loc:Point{x:1,y:0}});
         printer.print(Cell{content:Val(2),loc:Point{x:0,y:0}});
@@ -219,7 +217,7 @@ mod tests {
 
     #[test]
     fn test_with_wrong() {
-        let printer = APrinter::new("u4".to_string(),"c4".to_string(),2);
+        let mut printer = APrinter::new("u4".to_string(),"c4".to_string(),2);
         printer.print(Cell{content:Val(100),loc:Point{x:0,y:1}});
         printer.print(Cell{content:Wrong,loc:Point{x:1,y:0}});
         printer.print(Cell{content:Val(2),loc:Point{x:0,y:0}});
@@ -231,7 +229,7 @@ mod tests {
 
     #[test]
     fn test_changes() {
-        let printer = APrinter::new("u5".to_string(),"c5".to_string(),2);
+        let mut printer = APrinter::new("u5".to_string(),"c5".to_string(),2);
         let first =  Cell{content:Val(72),loc:Point{x:5,y:100}} ;
         let changes = vec![
             (first,
@@ -249,7 +247,7 @@ mod tests {
 
     #[test]
     fn test_changes_with_wrong() {
-        let printer = APrinter::new("u6".to_string(),"c6".to_string(),2);
+        let mut printer = APrinter::new("u6".to_string(),"c6".to_string(),2);
         let first =  Cell{content:Val(72),loc:Point{x:5,y:100}} ;
         let changes = vec![
             (first,
