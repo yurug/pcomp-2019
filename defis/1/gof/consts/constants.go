dee588f7647b9acc13280c46623cd14c80c6f09d
Samy Metadjer <samy.metadjer@gmail.com>
2019-02-17 22:35:58 +0100

    feat(consts): add package for constants

---
+++ defis/1/gof/consts/constants.go
@@ -0,0 +1,17 @@
+package consts
+
+const KIND_NUMBER = "NUMBER"
+const KIND_FORMULA = "FORMULA"
+const KIND_UNKNOWN = "UNKOWN"
+const SIZE_FORMULA = 5
+const EXPRESSION_FORMULA = `=#[(]\d+, \d+, \d+, \d+, \d+[)]`
+const BAD_FORMAT = "P"
+
+const BINARY_FILE = "binaries/binary"
+const DETAILS = "binaries/details"
+const FORMULAS_FILE = "binaries/formulas"
+const DEPENDENCIES_FILE = "binaries/dependencies"
+const CREATE_FILE = 1
+const OPEN_FILE = 0
+
+const LINE_DELIMITER = '\n'
