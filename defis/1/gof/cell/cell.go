92c553f7b825dc65655386bd128348913215280b
Samy Metadjer <samy.metadjer@gmail.com>
2019-02-18 10:49:31 +0100

    refactor(cell): moved cells into own package to avoid cycles

---
+++ defis/1/gof/cell/cell.go
@@ -0,0 +1,129 @@
+package cell
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/yurug/pcomp-2019/defis/1/gof/consts"
+)
+
+type Cell interface {
+	Coordinate() Coordinate
+	Value() string
+	Visited() bool
+	MarkVisit()
+}
+
+type Coordinate struct {
+	X, Y int
+}
+
+type Number struct {
+	position Coordinate
+	value    int
+	visited  bool
+}
+
+func NewNumber(x int, y int, v int) (*Number, error) {
+	if v < 0 || v > 255 {
+		return nil, fmt.Errorf("Invalid value for Number type : Must have 0 <= v <= 255")
+	}
+	return &Number{
+		position: Coordinate{x, y},
+		value:    v,
+		visited:  false,
+	}, nil
+}
+
+func (n *Number) Coordinate() Coordinate {
+	return n.position
+}
+
+func (n *Number) Value() string {
+	return strconv.Itoa(n.value)
+}
+
+func (n *Number) Visited() bool {
+	return n.visited
+}
+
+func (n *Number) MarkVisit() {
+	n.visited = true
+}
+
+type Formula struct {
+	position Coordinate
+	Start    Coordinate
+	End      Coordinate
+	Area     int
+	ToEval   int
+	FinalV   int
+	visited  bool
+}
+
+func NewFormula(r1 int, c1 int, r2 int, c2 int, v int, x int, y int) *Formula {
+	return &Formula{
+		position: Coordinate{x, y},
+		Start:    Coordinate{r1, c1},
+		End:      Coordinate{r2, c2},
+		Area:     (r2 - r1) * (c2 - c1),
+		ToEval:   v,
+		FinalV:   -1,
+		visited:  false,
+	}
+}
+
+func (f *Formula) Coordinate() Coordinate {
+	return f.position
+}
+
+func (f *Formula) Value() string {
+	if f.FinalV != -1 {
+		return strconv.Itoa(f.FinalV)
+	}
+	return "F"
+}
+
+func (f *Formula) Visited() bool {
+	return f.visited
+}
+
+func (f *Formula) MarkVisit() {
+	f.visited = true
+}
+
+func (f *Formula) nbCells() int {
+	return f.Area
+}
+
+func (f *Formula) decrementArea() {
+	f.Area--
+}
+
+type Unknown struct {
+	position Coordinate
+	visited  bool
+}
+
+func NewUnknown(x int, y int) *Unknown {
+	return &Unknown{
+		position: Coordinate{x, y},
+		visited:  false,
+	}
+}
+
+func (u *Unknown) Coordinate() Coordinate {
+	return u.position
+}
+
+func (u *Unknown) MarkVisit() {
+	u.visited = true
+}
+
+func (u *Unknown) Visited() bool {
+	return u.visited
+}
+
+func (u *Unknown) Value() string {
+	return consts.BAD_FORMAT
+}
---
+++ defis/1/gof/cell/fList.go
@@ -0,0 +1,105 @@
+package cell
+
+import (
+	"container/list"
+	"strconv"
+)
+
+/*FormulasMapping is a struct to store formulas and unknown cells
+**formula[key] has an integer as key to iterate through it easily
+**unknown[key] has a string as key representing [x,y]
+ */
+type FormulasMapping struct {
+	formula map[int]*Formula
+	unknown map[string]*Unknown
+}
+
+//CreateList takes as input 2 channelsm create a FormulasMap struct
+//fill it with the cells received in c, then send it to r
+func CreateList(c chan Cell, r chan *FormulasMapping) {
+	fl := newFormulasMapping()
+	fl.fillList(c)
+	r <- fl
+}
+
+func newFormulasMapping() *FormulasMapping {
+	return &FormulasMapping{formula: make(map[int]*Formula), unknown: make(map[string]*Unknown)}
+}
+
+func (fl *FormulasMapping) ListID() *list.List {
+	l := list.New()
+	for k := range fl.formula {
+		l.PushFront(k)
+	}
+	return l
+}
+
+//Formula returns the formula with id as key
+func (fl *FormulasMapping) Formula(id int) *Formula {
+	return fl.formula[id]
+}
+
+//Unknown returns the unknown with id as key
+func (fl *FormulasMapping) Unknown(id string) *Unknown {
+	return fl.unknown[id]
+}
+
+//Initialize the fList with cells given through channel c,
+//send a pointer to self to caller through channel r
+func (fl *FormulasMapping) fillList(c chan Cell) {
+	var formulaList = list.New()
+	var unknownList = list.New()
+	for f := range c {
+		switch f.(type) {
+		case *Formula:
+			insert(f.(*Formula), formulaList)
+		case *Unknown:
+			unknownList.PushBack(f.(*Unknown))
+		}
+	}
+	fl.createMaps(formulaList, unknownList)
+}
+
+// Insert Formula into a list, preserving formulas order (Start(X,Y))
+func insert(f *Formula, l *list.List) {
+	for e := l.Front(); e != nil; e = e.Next() {
+		if compareCoord(f.Start, e.Value.(*Formula).Start) == -1 {
+			continue
+		}
+		l.InsertBefore(f, e)
+		return
+	}
+	l.PushBack(f)
+}
+
+// Create maps of valid formulas and unknown cells from given lists
+func (fl *FormulasMapping) createMaps(validList *list.List, invalidList *list.List) {
+	var i = 0
+	for e := validList.Front(); e != nil; e = e.Next() {
+		fl.formula[i] = e.Value.(*Formula)
+		i++
+	}
+	for e := invalidList.Front(); e != nil; e = e.Next() {
+		element := e.Value.(*Unknown)
+		x := strconv.Itoa(element.Coordinate().X)
+		y := strconv.Itoa(element.Coordinate().Y)
+		fl.unknown[x+","+y] = e.Value.(*Unknown)
+	}
+}
+
+// Compare between two coordiantes (first X then Y) return 1 if c1 is GT, -1 if c1 is LT
+func compareCoord(c1 Coordinate, c2 Coordinate) int {
+	if c1.X > c2.X {
+		return 1
+	}
+	if c1.X < c2.X {
+		return -1
+	}
+	if c1.Y > c2.Y {
+		return 1
+	}
+	if c1.Y < c2.Y {
+		return -1
+	}
+	return 0
+}
