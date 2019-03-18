0d95878e795723e156e6d3bde953bc0753a3c571
n3f4s <tiv.crb@gmail.com>
2019-03-18 18:31:30 +0100

    Clean code

--- defis/2/aquamen/src/main.rs
+++ defis/2/aquamen/src/main.rs
@@ -20,7 +20,7 @@ mod area;
 
 use std::env;
 
-// use sched::schedule;
+use sched::schedule;
 
 use log::{Record, Level, Metadata, LevelFilter};
 
@@ -42,42 +42,12 @@ impl log::Log for MyLogger {
 }
 
 fn main() {
-    // log::set_logger(&MY_LOGGER).unwrap();
-    // FIXME better handling of logging
-    // log::set_max_level(LevelFilter::Trace);
+    log::set_logger(&MY_LOGGER).unwrap();
+    log::set_max_level(LevelFilter::Trace);
 
     let bench = bench::bench::start_bench();
 
-    // use tree::Tree;
-    // use data::Data::*;
-    // use data::Point;
-    // use data::Cell;
+    let args: Vec<String> = env::args().collect();
 
-    // let num_cell: u8 = 15;
-
-    // let mut t = Tree::with_size(num_cell as u64);
-
-    // let mut cells = Vec::with_capacity((num_cell as usize) * (num_cell as usize));
-
-    // for i in 0..num_cell {
-    //     for j in 0..num_cell {
-    //         cells.push((i, j))
-    //     }
-    // }
-
-    // for (i, j) in &cells {
-    //     t.add_cell(Cell{
-    //         loc: Point{x:*i as u64, y:*j as u64},
-    //         content: Val(i+j),
-    //     });
-    // }
-    // for (i, j) in &cells {
-    //     println!("{:?}", t.get_cell(Point{x:*i as u64, y:*j as u64}));
-    // }
-    // println!("{}", t.size());
-
-    // let args: Vec<String> = env::args().collect();
-
-
-    // schedule(&args[1], &args[2], &args[3], &args[4], bench);
+    schedule(&args[1], &args[2], &args[3], &args[4], bench);
 }
