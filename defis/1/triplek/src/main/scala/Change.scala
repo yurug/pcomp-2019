3e6e9c259059b7c3f4de74ab3279bf3eef4a5b36
Karnaj <yoan.geran@hotmail.fr>
2019-03-22 03:44:16 +0100

    Documenta.

--- defis/1/triplek/src/main/scala/Change.scala
+++ defis/1/triplek/src/main/scala/Change.scala
@@ -5,6 +5,12 @@ import scala.collection.mutable.HashMap
 import utils._
 
 
+/** Abstract class to repreesent a change of a cell.
+  *
+  * @contructor Creates a new Change with a position and a value.
+  * @param p The position of this `Change`.
+  * @param y The value of the associated cell.
+  */
 abstract class Change(val p: Position, var v: Int) {
 
   var affecteds: List[Change] = List()
@@ -17,10 +23,31 @@ abstract class Change(val p: Position, var v: Int) {
   var oldCorrect: Boolean = true
   var has_changed: Boolean = false
 
+  /** toString methods (used by `println`, etc.).
+    *
+    * @return A `String` corresponding to the change value (or "P" if incorrect
+    *         `Change`).
+    */
   override def toString: String = if(correct) s"${v}" else "P"
+
+  /** Checks if a `Change` can affect the current `Change` (it means that
+    * it is a depedencies).
+    *
+    * @param c The `Change` to test.
+    * @return `true` if `c` is a dependecies of the current `Change`.
+    */
+
   def depends_on(c: Change): Boolean = false
+
+  /** Propagate a `Change`.
+    *
+    * @param c The `Change` to propagate.
+    * @return viewed A HashMap which permits to know to which `Change` it has
+    *         already been propagated (a HashSet seems more reasonable?).
+    */
   def propagate(c: Change, viewed: HashMap[Change, Boolean]): Unit = ()
 
+  /** Evaluate the `Change`. */
   def evaluate = {
     var viewed: HashMap[Change, Boolean] =
       new HashMap[Change, Boolean]()  { override def default(key:Change) = false }
@@ -29,6 +56,12 @@ abstract class Change(val p: Position, var v: Int) {
     affecteds.foreach { c => c.propagate(this, viewed) }
   }
 
+  /** Propagate an error. Indeed, if you are not correct, all the `Changes` that
+    * depend on you are also incorrect.
+    *
+    * @return viewed A HashMap which permits to know to which `Change` it has
+    *         already been propagated (a HashSet seems more reasonable?).
+    */
   def propagateError(viewed: HashMap[Change, Boolean]): Unit = {
     if(!viewed(this)) {
       if(correct) {
@@ -42,6 +75,9 @@ abstract class Change(val p: Position, var v: Int) {
     }
   }
 
+  /** Init the `Change` (for evaluation). By default it is correct, and has
+      as value it initial value with A.
+    */
   def init: Unit = {
     hasChanged = false
     correct = true
@@ -113,13 +149,26 @@ extends Change(pos, value) {
   }
 }
 
+/** Companion object of the class Object, to simulate class methods. */
 object Change {
+
+  /** Sort a list of `BChange` by block position (lexicographic order on
+    * (top-left corner, bottom-right corner)).
+    *
+    * @param l The list of `BChange` to sort.
+    * @return The sorted list.
+    */
   def sortByBlockPosition(l: List[BChange]): List[BChange] = {
     l.sortBy { c =>
       (c.b.bottomRight.x, c.b.bottomRight.y, c.b.topLeft.x, c.b.topLeft.y)
     }
   }
 
+  /** Split a list of `Change` into the `AChange` part and the `BChange` part.
+    *
+    * @param l The list of `Change` to split.
+    * @return The list of `AChange` and the list of `BChange` present in `l`.
+    */
   def split(changes: List[Change]): (List[AChange], List[BChange]) = {
     val (la, lb): (List[Change], List[Change]) = changes.partition {
       case c: AChange => true
